ef13a8432f62eb38fa63cbda5a9a7e21a8de08a1
Zahid Hussain <zahid4307@gmail.com>
2024-05-25 16:38:36 +0500

    real state mern project , design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import "./App.css";
+import Companies from "./components/companies/Companies";
+import Residency from "./components/residency/Residency";
+import AccordianInterface from "./components/accordianInterface/AccordianInterface";
+import Contact from "./components/contact/Contact";
+import GetStarted from "./components/getStarted/GetStarted";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="">
+          <Header />
+          <Hero />
+        </div>
+      </div>
+      <Companies />
+      <Residency />
+      <AccordianInterface />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
   );
 }
 
