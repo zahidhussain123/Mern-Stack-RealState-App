01e0200bdf14401d82b783556f1fafad8f5a1423
Zahid Hussain <zahid4307@gmail.com>
2024-06-29 17:37:03 +0500

    validatelogin changes

--- src/layout/ProtectedRoute.jsx
+++ src/layout/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import UserDetailsContext from '../context/UserDetailsContext'
 import { Outlet, Navigate } from 'react-router-dom'
+import useAuthCheck from '../hooks/useAuthCheck'
 
 const ProtectedRoute = () => {
-   const {userDetails: {token}} = useContext(UserDetailsContext)
-  return token ? <Outlet /> : <Navigate to={"/"} />
+//    const {userDetails: {token}} = useContext(UserDetailsContext)
+  const {validateLogin} =  useAuthCheck()
+  return validateLogin() ? <Outlet /> : <Navigate to={"/"} />
 }
 
 export default ProtectedRoute
