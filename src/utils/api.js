9ae7f73a9cd0d5f77ace316769ff27125aa3baef
Zahid Hussain <zahid4307@gmail.com>
2024-06-29 13:02:42 +0500

    url issue again

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import { toast } from "react-toastify"
 
 export const api = axios.create({
-  baseURL: "https://mern-stack-real-state-app-server.vercel.app//api"
+  baseURL: "https://mern-stack-real-state-app-server.vercel.app/api"
 })
 
 export const getAllProperties = async () => {
